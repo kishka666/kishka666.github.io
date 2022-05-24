975b809cf89a59e6cd0b9ba7e4724fc58d6483d5
kishka666 <105529378+kishka666@users.noreply.github.com>
2022-05-24 14:42:04 +0700

    Add files via upload

---
+++ скрипт/scripti.js
@@ -0,0 +1,12 @@
+function show ( name )
+{
+  var elem = document.getElementById(name);
+  if ( elem )
+    elem.style.display = "block";
+}
+function show1 ( name )
+{
+  var elem = document.getElementById(name);
+  if ( elem )
+    elem.style.display = "none";
+}
